f7a7e9d65e110e4ce6be06f10b3710ddc19f904d
Revan <abusovrvan34@gmail.com>
2026-05-07 11:07:56 +0400

    trigger pr

--- src/main/java/org/example/dockercompose/DockerComposeApplication.java
+++ src/main/java/org/example/dockercompose/DockerComposeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerComposeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DockerComposeApplication.class, args);
-    }
+        SpringApplication.run(DockerComposeApplication.class, args);}
 
 }
